efd07b44873df51f8ab0d0b46dcc0e000c3de786
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 08:05:46 +0100

    Upgraded to Molecule 1.10.3
    Fixed flake8 linter warning.

--- tests/test_role.py
+++ tests/test_role.py
@@ -6,5 +6,6 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 def test_mvn_color(Command):
     assert '3.3.9' in Command.check_output('mvn --version')
 
+
 def test_mvn_debug(Command):
     assert Command('which mvnDebug').rc == 0
