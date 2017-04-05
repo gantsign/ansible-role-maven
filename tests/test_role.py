3752677a1f7f28147e90eb785fcc3c0d2d6766a6
John Freeman <john.freeman.opensource@gmail.com>
2017-04-05 19:47:52 +0100

    Added test for path and attributes of executables (#67)
    Check the path, file ownership and permissions.

--- tests/test_role.py
+++ tests/test_role.py
@@ -1,3 +1,5 @@
+import pytest
+
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
@@ -9,3 +11,22 @@ def test_mvn(Command):
 
 def test_mvn_debug(Command):
     assert Command('which mvnDebug').rc == 0
+
+
+@pytest.mark.parametrize('command', [
+    'mvn',
+    'mvnDebug'
+])
+def test_commands_installed(Command, File, command):
+
+    maven_home = Command.check_output('find %s | grep --color=never -E %s',
+                                      '/opt/maven',
+                                      'apache-maven-3\\.3\\.[0-9]+$')
+
+    command_file = File(maven_home + '/bin/' + command)
+
+    assert command_file.exists
+    assert command_file.is_file
+    assert command_file.user == 'root'
+    assert command_file.group == 'root'
+    assert oct(command_file.mode) == '0755'
